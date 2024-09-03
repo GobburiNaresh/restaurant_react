e03e7bb7f244f7af66d1f4c4d4098d4ada34d3e7
GobburiNaresh <nareshgobburi2001@gmail.com>
2024-09-03 21:49:21 +0530

    latest update

--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -1,25 +1,45 @@
-import {useContext} from 'react';
+import { useContext, useEffect, useState } from 'react';
 
-import CardIcon from '../Cart/CardIcon';
+import CartIcon from '../Cart/CardIcon';
 import CartContext from '../../store/card-context';
 import classes from './HeaderCartButton.module.css';
 
-const HeaderCardButton = props => {
-    const cartCtx = useContext(CartContext);
+const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+  const cartCtx = useContext(CartContext);
 
-    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
-        return curNumber + item.amount;
-    },0);
+  const { items } = cartCtx;
 
-    return (
-        <button className={classes.button} onClick={props.onClick}>
-            <span className={classes.icon}>
-                <CardIcon/>
-            </span>
-            <span>Your Cart</span>
-            <span className={classes.badge}>{numberOfCartItems}</span>
-        </button>
-    );
-}
+  const numberOfCartItems = items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
 
-export default HeaderCardButton;
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
+  return (
+    <button className={btnClasses} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
